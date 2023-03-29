49000572b51c4232dfbaa20c76f1fb766b1ddae9
Gonaline <aline-gonce_student2022@wilder.school>
2023-03-29 20:11:54 +0200

    (client): add styled reset to index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Reset } from 'styled-reset';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Reset />
     <BrowserRouter>
       <App />
     </BrowserRouter>
