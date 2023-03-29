763b835d2fa286ff0da1bf80fcbda17f8b9cb68b
Gonaline <aline-gonce_student2022@wilder.school>
2023-03-29 18:44:22 +0200

    (client): create main pages

---
+++ client/src/pages/project2.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponentElement, ReactElement } from 'react';
+
+const Project2 = (): FunctionComponentElement<ReactElement> => {
+  return <h3>Project2</h3>;
+};
+export default Project2;
