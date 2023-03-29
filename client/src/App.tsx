d9760c6828f2206479da65e8f3daf96f0945468e
Gonaline <aline-gonce_student2022@wilder.school>
2023-03-29 19:06:11 +0200

    (client): add first routes to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/about';
+import Project1 from './pages/project1';
+import Project2 from './pages/project2';
+import Project3 from './pages/project3';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>PORTFOLIO</h1>
-    </div>
+    <main>
+      <Routes>
+        <Route path='*' element={<About />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Project1 />} />
+        <Route path='/rock-paper-or-scissors' element={<Project1 />} />
+        <Route path='/modeling-gallery' element={<Project2 />} />
+        <Route path='/stickers-shop' element={<Project3 />} />
+      </Routes>
+    </main>
   );
 }
 
