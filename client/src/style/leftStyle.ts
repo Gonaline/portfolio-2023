b288c12d15cf43f98395d0b794faa70bdeaae096
Gonaline <aline-gonce_student2022@wilder.school>
2023-03-30 18:52:15 +0200

    client): create left component & add style

---
+++ client/src/style/leftStyle.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export default styled.section`
+  background-color: var(--darkColor);
+  width: 30vw;
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  div {
+    margin-bottom: 10vh;
+  }
+  p,
+  h2,
+  h4 {
+    margin: 0 2.5vw 0 2.5vw;
+  }
+  .subTitle {
+    margin-bottom: 2vh;
+    opacity: 0.5;
+  }
+  p,
+  h4 {
+    color: var(--white);
+  }
+  h2 {
+    color: ${(props: any): string | undefined => {
+      return `${props.color}`;
+    }};
+    margin-bottom: 6vh;
+  }
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    width: 100vw;
+    min-height: 77vh;
+    margin-top: 14vh;
+    align-items: flex-end;
+    justify-content: space-between;
+    h2 {
+      padding-top: 2vh;
+    }
+    h2,
+    p,
+    h4 {
+      margin-top: 6vw;
+      margin-right: 6vw;
+      margin-left: 6vw;
+    }
+  }
+`;
