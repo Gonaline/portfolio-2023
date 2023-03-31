122dd0d08780dc45e3485377c3966f2590f2edb8
Gonaline <aline-gonce_student2022@wilder.school>
2023-03-31 14:06:48 +0200

    (client): add CtxProvider to index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,17 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './style/globalStyle';
+import { CtxProvider } from './context/project1Ctx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Reset />
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CtxProvider>
+      <Reset />
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CtxProvider>
   </React.StrictMode>
 );
 
