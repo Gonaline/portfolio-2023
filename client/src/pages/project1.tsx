4b7e7e54e0e287d4af982bed096681c183a6b23d
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-02 13:06:35 +0200

    client/project1: update project1 page with data of game & counter

--- client/src/pages/project1.tsx
+++ client/src/pages/project1.tsx
@@ -1,13 +1,35 @@
-import { FunctionComponentElement, ReactElement } from 'react';
+import {
+  FunctionComponentElement,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import Left from '../components/left';
+import Counter from '../components/project1/counter';
 import Game from '../components/project1/game';
 import ProjectTitle from '../components/projectTitle';
+import project1Ctx from '../context/project1Ctx';
 import pageData from '../data/pages';
+import dataGame from '../data/project1';
 import DataPage from '../interfaces/dataPage';
 import Project1Style from '../style/project1/project1Style';
 
 const Project1 = (): FunctionComponentElement<ReactElement> => {
+  const { userChoice, computer } = useContext(project1Ctx);
+
+  const [computerCounter, setComputerCounter] = useState<number>(0);
+  const [userCounter, setUserCounter] = useState<number>(0);
+
   const projectData: DataPage = pageData.filter((e) => e.id === 'PROJECT1')[0];
+
+  useEffect(() => {
+    if (!userChoice || !computer) return;
+    const game = dataGame.filter((e) => e.id === userChoice + computer)[0];
+    setUserCounter(game.userPoint + userCounter);
+    setComputerCounter(game.computerPoint + computerCounter);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [computer, userChoice]);
 
   return (
     <>
@@ -22,6 +44,11 @@ const Project1 = (): FunctionComponentElement<ReactElement> => {
         <Game
           buttonColor={projectData.textColor}
           onHoverButtonColor={projectData.otherColor}
+        />
+        <Counter
+          color={projectData.textColor}
+          userCounter={userCounter}
+          computerCounter={computerCounter}
         />
       </Project1Style>
     </>
