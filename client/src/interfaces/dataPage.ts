3f8d18ddcb9a483f8afa1a379b533c3c99f80f18
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-02 23:11:42 +0200

    client/pages: fix data page interface

--- client/src/interfaces/dataPage.ts
+++ client/src/interfaces/dataPage.ts
@@ -1,11 +1,11 @@
-export default interface DataPage {
-  id: string;
-  name: string;
+export default interface IDataPage {
   title: string;
   subtitle: string;
   description: string;
-  textColor?: string | undefined;
-  backgroundColor?: string | undefined;
-  headerColor?: string | undefined;
-  otherColor?: string | undefined;
+  textColor: string;
+  backgroundColor: string;
+  headerAndFooterColor: string;
+  buttonColor: string;
+  buttonOnHoverColor: string;
+  otherColor: string;
 }
