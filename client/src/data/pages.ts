3c3c3b35d4131208c8691939dcd210e0e70c070d
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-02 23:10:57 +0200

    client/pages: update data of pages

--- client/src/data/pages.ts
+++ client/src/data/pages.ts
@@ -1,18 +1,28 @@
-import DataPage from '../interfaces/dataPage';
-
-const dataPages: DataPage[] = [
-  {
-    id: 'PROJECT1',
-    name: 'ABOUT',
+const page = {
+  about: {
+    title: 'ABOUT',
+    subtitle: 'ABOUT',
+    description:
+      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    textColor: 'red',
+    backgroundColor: 'pink',
+    headerAndFooterColor: 'yellow',
+    buttonColor: '',
+    buttonOnHoverColor: '',
+    otherColor: '',
+  },
+  project1: {
     title: 'ROCK, PAPER or SCISSORS ?',
     subtitle: 'PROJECT #1',
     description:
       'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     textColor: '#00cc99',
     backgroundColor: '#97EAD5',
-    headerColor: '#00cc99',
+    headerAndFooterColor: '#00cc99',
+    buttonColor: '#00cc99',
+    buttonOnHoverColor: '#F36653',
     otherColor: '#F36653',
   },
-];
+};
 
-export default dataPages;
+export default page;
