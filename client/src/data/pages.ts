ed202818b5127c42109ceb328e4a3d947fde3107
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-03 22:37:42 +0200

    client/fix: update data of pages : grouping of colours in a theme object

--- client/src/data/pages.ts
+++ client/src/data/pages.ts
@@ -4,24 +4,28 @@ const page = {
     subtitle: 'ABOUT',
     description:
       'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    textColor: 'red',
-    backgroundColor: 'pink',
-    headerAndFooterColor: 'yellow',
-    buttonColor: '',
-    buttonOnHoverColor: '',
-    otherColor: '',
+    theme: {
+      textColor: 'red',
+      backgroundColor: 'pink',
+      headerAndFooterColor: 'yellow',
+      buttonColor: '',
+      buttonOnHoverColor: '',
+      otherColor: '',
+    },
   },
   project1: {
     title: 'ROCK, PAPER or SCISSORS ?',
     subtitle: 'PROJECT #1',
     description:
       'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    textColor: '#00cc99',
-    backgroundColor: '#97EAD5',
-    headerAndFooterColor: '#00cc99',
-    buttonColor: '#00cc99',
-    buttonOnHoverColor: '#F36653',
-    otherColor: '#F36653',
+    theme: {
+      textColor: '#00cc99',
+      backgroundColor: '#97EAD5',
+      headerAndFooterColor: '#00cc99',
+      buttonColor: '#00cc99',
+      buttonOnHoverColor: '#F36653',
+      otherColor: '#F36653',
+    },
   },
 };
 
