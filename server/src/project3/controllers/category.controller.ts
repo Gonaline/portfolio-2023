ac625f915449eef564f5a9d410dbf49efcea2569
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-06 20:44:04 +0200

    server: wip

--- server/src/project3/controllers/category.controller.ts
+++ server/src/project3/controllers/category.controller.ts
@@ -6,6 +6,18 @@ import ICategory from '../interfaces/category';
 class CategoryController {
   constructor(private categoryManager = new CategoryManager()) {}
 
+  public findAllCategoriesWithConvertName = async (
+    _req: Request,
+    res: Response
+  ) => {
+    return await this.categoryManager.findAll().then((result) => {
+      result.forEach((e: ICategory) => {
+        e.convert_category_name = convertText(e.category_name);
+      });
+      return result;
+    });
+  };
+
   public getCategories = async (_req: Request, res: Response) => {
     return await this.categoryManager
       .findAll()
@@ -27,28 +39,34 @@ class CategoryController {
 
   public getProductsByCategory = async (req: Request, res: Response) => {
     const convertName = req.params.convertName;
-    // const allCategories = await this.categoryManager.findAll();
-    // const [categoryName] = allCategories.filter(
-    //   (e) => e.convert_category_name === convertName
-    // );
-    // console.log(`categoryName: ${categoryName}`);
+    const categories = await this.findAllCategoriesWithConvertName(req, res);
 
-    const allCategories = this.getCategories(req, res);
-    console.log(`allCategories: ${JSON.stringify(allCategories)}`);
-
-    return await this.categoryManager
-      .findOne(convertName)
-      .then((result) => {
-        if (!result) {
-          res.sendStatus(404);
-        } else {
-          res.status(200).json(result);
-        }
-      })
-      .catch((err: any) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
+    if (!categories || categories.length === 0) {
+      return [];
+    } else {
+      const [category] = categories.filter(
+        (e) => e.convert_category_name === convertName
+      );
+      let id: string;
+      if (category === undefined) {
+        id = '';
+      } else {
+        id = category.id.toString();
+      }
+      return await this.categoryManager
+        .findProductsByCategory(id)
+        .then((result) => {
+          if (!result) {
+            res.sendStatus(404);
+          } else {
+            res.status(200).json(result);
+          }
+        })
+        .catch((err: any) => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    }
   };
 }
 
--- server/src/project3/models/category.manager.ts
+++ server/src/project3/models/category.manager.ts
@@ -3,7 +3,9 @@ import connection from '../database';
 import { Pool } from 'mysql2/promise';
 import { TABLE } from '../enums/table.enum';
 import ICategory from '../interfaces/category';
+
 const category = TABLE.CATEGORY;
+const product_category = TABLE.PRODUCT_CATEGORY;
 
 class CategoryManager {
   public connection: Pool;
@@ -17,15 +19,14 @@ class CategoryManager {
     return rows as ICategory[];
   }
 
-  public async findOne(convertName: string): Promise<ICategory[]> {
-    const sql = `SELECT * FROM ${category} WHERE id = ?`;
-    //     SELECT pc.product_id, c.category_name
-    // FROM stickers_shop.product_category AS pc
-    // INNER JOIN stickers_shop.category AS c
-    // WHERE pc.category_id = c.id
-    // AND c.category_name = 'les Savanimo';
+  public async findProductsByCategory(id: string): Promise<ICategory[]> {
+    const sql = `SELECT pc.product_id, c.id, c.category_name
+    FROM ${product_category} AS pc
+    INNER JOIN ${category} AS c
+    WHERE pc.category_id = c.id
+    AND pc.category_id = ?`;
 
-    const [rows] = await this.connection.execute(sql, [convertName]);
+    const [rows] = await this.connection.execute(sql, [id]);
     return rows as ICategory[];
   }
 }
