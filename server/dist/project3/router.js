ac8fc12b9a9eba7196cf37d43914c63b4d0bf952
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-06 22:12:50 +0200

    server: update dist

--- server/dist/project3/router.js
+++ server/dist/project3/router.js
@@ -5,8 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const product_controller_1 = __importDefault(require("./controllers/product.controller"));
+const category_controller_1 = __importDefault(require("./controllers/category.controller"));
 const Project3Router = (0, express_1.Router)();
 const productController = new product_controller_1.default();
+const categoryController = new category_controller_1.default();
 Project3Router.get('/products', productController.getProducts);
 Project3Router.get('/product/:id', productController.getProductById);
+Project3Router.get('/categories', categoryController.getCategories);
+Project3Router.get('/category/:convertName', categoryController.getProductsByCategory);
 exports.default = Project3Router;
