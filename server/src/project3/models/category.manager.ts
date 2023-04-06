925c65146e2819bbf8ab82b60d3d0ebfb5b4f21c
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-06 22:12:27 +0200

    server: create category scripts

--- server/src/project3/models/category.manager.ts
+++ server/src/project3/models/category.manager.ts
@@ -1,9 +1,11 @@
-import IProduct from '../interfaces/product';
 import connection from '../database';
 import { Pool } from 'mysql2/promise';
 import { TABLE } from '../enums/table.enum';
 import ICategory from '../interfaces/category';
+import { COLLECTION } from '../enums/collection.enum';
+import IProductsByCategory from '../interfaces/productsByCategory';
 
+const product = TABLE.PRODUCT;
 const category = TABLE.CATEGORY;
 const product_category = TABLE.PRODUCT_CATEGORY;
 
@@ -19,15 +21,36 @@ class CategoryManager {
     return rows as ICategory[];
   }
 
-  public async findProductsByCategory(id: string): Promise<ICategory[]> {
-    const sql = `SELECT pc.product_id, c.id, c.category_name
-    FROM ${product_category} AS pc
-    INNER JOIN ${category} AS c
-    WHERE pc.category_id = c.id
-    AND pc.category_id = ?`;
+  public async findProductsByCategory(
+    id: string,
+    convertName: string
+  ): Promise<IProductsByCategory[]> {
+    let sql = '';
 
-    const [rows] = await this.connection.execute(sql, [id]);
-    return rows as ICategory[];
+    if (convertName === COLLECTION.ALL_COLLECTIONS_CONVERT_NAME) {
+      sql = `SELECT DISTINCT(pc.product_id), c.id AS category_id, c.category_name, p.product_name, IFNULL(p.first_image, CONCAT(pc.product_id, ".png")) AS first_image
+      FROM ${product_category} AS pc
+      INNER JOIN ${category} AS c
+      ON pc.category_id = c.id
+      INNER JOIN ${product} AS p
+      ON pc.product_id = p.id
+      ORDER BY p.product_order`;
+
+      const [rows] = await this.connection.execute(sql);
+      return rows as IProductsByCategory[];
+    } else {
+      sql = `SELECT pc.product_id, c.id AS category_id, c.category_name, p.product_name, IFNULL(p.first_image, CONCAT(pc.product_id, ".png")) AS first_image
+      FROM ${product_category} AS pc
+      INNER JOIN ${category} AS c
+      ON pc.category_id = c.id
+      INNER JOIN ${product} AS p
+      ON pc.product_id = p.id
+      WHERE pc.category_id = ?
+      ORDER BY p.product_order`;
+
+      const [rows] = await this.connection.execute(sql, [id]);
+      return rows as IProductsByCategory[];
+    }
   }
 }
 
