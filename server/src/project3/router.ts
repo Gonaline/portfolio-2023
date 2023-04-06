6aa24da250ab12f60ed86cd01f90015c979024d9
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-06 15:10:55 +0200

    server: add project3 route to get one product

--- server/src/project3/router.ts
+++ server/src/project3/router.ts
@@ -6,6 +6,6 @@ const Project3Router = Router();
 const productController = new ProductController();
 
 Project3Router.get('/products', productController.getProducts);
-// Project3Router.get('/product/:id', productController.getProductById);
+Project3Router.get('/product/:id', productController.getProductById);
 
 export default Project3Router;
