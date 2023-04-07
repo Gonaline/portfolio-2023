418f1aee28d3a458464b2091021d553a3962a2f2
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-07 12:08:15 +0200

    client: add left & categoryNav components to page of project3

--- client/src/pages/project3.tsx
+++ client/src/pages/project3.tsx
@@ -1,6 +1,31 @@
-import { FunctionComponentElement, ReactElement } from 'react';
+import {
+  FunctionComponentElement,
+  ReactElement,
+  useContext,
+  useEffect,
+} from 'react';
+import Left from '../components/left';
+import GlobalCtx from '../context/globalCtx';
+import pageData from '../data/pages';
+import { PAGE } from '../enums/page.enum';
+import CategoryNav from '../components/project3/categoryNav';
+import Project3Style from '../style/project3/project3Style';
 
 const Project3 = (): FunctionComponentElement<ReactElement> => {
-  return <h3>Project3</h3>;
+  const { setPageData } = useContext(GlobalCtx);
+
+  useEffect(() => {
+    setPageData(pageData[PAGE.PROJECT3]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Left />
+      <Project3Style>
+        <CategoryNav />
+      </Project3Style>
+    </>
+  );
 };
 export default Project3;
