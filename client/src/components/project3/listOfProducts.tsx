83a0bab24e1b1c831b903a9e021b6b3a5c0416ac
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-07 18:56:12 +0200

    client: display products images

--- client/src/components/project3/listOfProducts.tsx
+++ client/src/components/project3/listOfProducts.tsx
@@ -1,9 +1,27 @@
-import { FunctionComponentElement, ReactElement } from 'react';
+import { FunctionComponentElement, ReactElement, useContext } from 'react';
+import project3Ctx from '../../context/project3Ctx';
+import { NavLink } from 'react-router-dom';
+import { PAGE } from '../../enums/page.enum';
 
 const ListOfProducts = (): FunctionComponentElement<ReactElement> => {
+  const { collectionData, collectionConvertName } = useContext(project3Ctx);
   return (
     <>
-      <p>ListOfProducts</p>
+      {collectionData.map((e) => (
+        <NavLink
+          to={`/${PAGE.PROJECT3_PATH}/${collectionConvertName}/${e.product_id}`}
+          className='link'
+          key={e.product_id}
+        >
+          <div key={e.first_image}>
+            <img
+              src={require(`../../assets/pictures/project3/products/${e.first_image}`)}
+              alt={e.product_name}
+            />
+            <p>{e.product_name}</p>
+          </div>
+        </NavLink>
+      ))}
     </>
   );
 };
