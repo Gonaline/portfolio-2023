7cf44c1930072769004449357e6dce5a647f809b
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-07 18:59:38 +0200

    client: add list of products to project3 page

--- client/src/pages/project3.tsx
+++ client/src/pages/project3.tsx
@@ -11,12 +11,17 @@ import pageData from '../data/pages';
 import { PAGE } from '../enums/page.enum';
 import CategoryNav from '../components/project3/categoryNav';
 import Project3Style from '../style/project3/project3Style';
+import ListOfProducts from '../components/project3/listOfProducts';
+import project3Ctx from '../context/project3Ctx';
+import { COLLECTION } from '../enums/project3/collection.enum';
 
 const Project3 = (): FunctionComponentElement<ReactElement> => {
   const { setPageData } = useContext(GlobalCtx);
+  const { setCollectionConvertName } = useContext(project3Ctx);
 
   useEffect(() => {
     setPageData(pageData[PAGE.PROJECT3]);
+    setCollectionConvertName(COLLECTION.ALL_COLLECTIONS_CONVERT_NAME);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -25,6 +30,7 @@ const Project3 = (): FunctionComponentElement<ReactElement> => {
       <Left />
       <Project3Style>
         <CategoryNav />
+        <ListOfProducts />
         <Outlet />
       </Project3Style>
     </>
