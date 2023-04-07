a02ad0b89471edf179ffb0c69d3260c88e1c6df7
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-07 20:15:41 +0200

    client: update interface of IProductsByCategory with convert_category_name, convert_product_name, price & price_option

--- client/src/interfaces/project3/productsByCollection.ts
+++ client/src/interfaces/project3/productsByCollection.ts
@@ -4,4 +4,8 @@ export default interface IProductsByCollection {
   category_name: string;
   product_name: string;
   first_image: string;
+  convert_category_name: string;
+  convert_product_name: string;
+  price: number;
+  price_option: number | null;
 }
