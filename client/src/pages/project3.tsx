6b827cb21b2a89a2e6930fd7c3efc5b2fdb4a757
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-09 20:43:29 +0200

    client: delete unused setState

--- client/src/pages/project3.tsx
+++ client/src/pages/project3.tsx
@@ -6,23 +6,20 @@ import {
 } from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalCtx from '../context/globalCtx';
-import project3Ctx from '../context/project3Ctx';
 import pageData from '../data/pages';
 import { PAGE } from '../enums/page.enum';
-import { COLLECTION } from '../enums/project3/collection.enum';
 import Left from '../components/left';
 import CategoryNav from '../components/project3/categoryNav';
 import Project3Style from '../style/project3/project3Style';
 
 const Project3 = (): FunctionComponentElement<ReactElement> => {
   const { setPageData } = useContext(GlobalCtx);
-  const { setCollectionConvertName } = useContext(project3Ctx);
 
   useEffect(() => {
     setPageData(pageData[PAGE.PROJECT3]);
-    setCollectionConvertName(COLLECTION.ALL_COLLECTIONS_CONVERT_NAME);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
       <Left />
