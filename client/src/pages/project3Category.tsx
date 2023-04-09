04cb578b1e439d712b6961ff06b045f403d01c58
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-09 19:14:14 +0200

    client: wip

--- client/src/pages/project3Category.tsx
+++ client/src/pages/project3Category.tsx
@@ -1,37 +1,47 @@
-import {
-  FunctionComponentElement,
-  ReactElement,
-  useContext,
-  useMemo,
-} from 'react';
-import Left from '../components/left';
-import CategoryNav from '../components/project3/categoryNav';
-import Project3Style from '../style/project3/project3Style';
-import ListOfProducts from '../components/project3/listOfProducts';
-import { useParams } from 'react-router-dom';
+import { FunctionComponentElement, ReactElement, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import project3Ctx from '../context/project3Ctx';
+import {
+  CardStyle,
+  ListOfProductsStyle,
+} from '../style/project3/listOfProductsStyle';
+import { PAGE } from '../enums/page.enum';
 
 const Project3Category = (): FunctionComponentElement<ReactElement> => {
-  const { category } = useParams<{ category?: string }>();
-  const { collectionConvertName, setCollectionConvertName } =
-    useContext(project3Ctx);
+  const { collectionConvertName } = useContext(project3Ctx);
+  const { productsByCollection } = useContext(project3Ctx);
+  const navigate = useNavigate();
 
-  useMemo(() => {
-    if (category) {
-      setCollectionConvertName(category);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category]);
+  const updateData: any = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = event.currentTarget;
+    navigate(`${PAGE.PROJECT3_PATH}/${value}`);
+  };
 
-  console.log(category);
   return (
     <>
-      <Left />
-      <Project3Style>
-        <CategoryNav />
-        <h4>{collectionConvertName}</h4>
-        <ListOfProducts />
-      </Project3Style>
+      <h4>{collectionConvertName}</h4>
+      <ListOfProductsStyle>
+        {productsByCollection.map((e) => (
+          <CardStyle
+            type='button'
+            key={e.product_id}
+            value={e.convert_product_name}
+            onClick={() => {
+              updateData();
+            }}
+          >
+            <img
+              src={require(`../assets/pictures/project3/products/${e.first_image}`)}
+              alt={e.product_name}
+            />
+            <div className='productName'>
+              <p>{e.product_name}</p>
+            </div>
+          </CardStyle>
+        ))}
+      </ListOfProductsStyle>
     </>
   );
 };
