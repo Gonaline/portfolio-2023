cd956999bea7d8b22dfdaea9c2453f1c46201ca3
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-10 20:14:35 +0200

    server: wip productController

--- server/src/project3/controllers/product.controller.ts
+++ server/src/project3/controllers/product.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 import ProductManager from '../models/product.manager';
+import OptionManager from '../models/option.manager';
 
 class ProductController {
-  constructor(private productManager = new ProductManager()) {}
+  constructor(
+    private productManager = new ProductManager(),
+    private optionManager = new OptionManager()
+  ) {}
 
   public getProducts = async (_req: Request, res: Response) => {
     return await this.productManager
@@ -27,19 +31,42 @@ class ProductController {
 
   public getProductById = async (req: Request, res: Response) => {
     const id: string = req.params.id;
-    return await this.productManager
+    const product = await this.productManager
       .findOne(id)
       .then((result) => {
         if (!result) {
           res.sendStatus(404);
         } else {
-          res.status(200).json(result[0]);
+          return result;
         }
       })
       .catch((err: any) => {
         console.error(err);
         res.sendStatus(500);
       });
+
+    if (product && product[0].option_id !== null) {
+      let options = await this.optionManager
+        .findOne(id)
+        .then((result) => {
+          if (!result) {
+            res.sendStatus(404);
+          } else {
+            const options = {
+              name: product[0].option_name,
+              detail: result,
+            };
+            return options;
+          }
+        })
+        .catch((err: any) => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+      product[0].options = options;
+    }
+
+    return res.status(200).json(product);
   };
 }
 
