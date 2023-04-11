f16c0fa7eea1c8b4a78690455eb9500ab856def4
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-11 18:25:24 +0200

    server: add all routes

--- server/src/project3/router.ts
+++ server/src/project3/router.ts
@@ -1,19 +1,42 @@
 import { Router } from 'express';
 import ProductController from './controllers/product.controller';
 import CategoryController from './controllers/category.controller';
+import TechnicalFileController from './controllers/technicalFile.controller';
+import OptionController from './controllers/option.controller';
+import ColorController from './controllers/color.controller';
 
 const Project3Router = Router();
 
 const productController = new ProductController();
 const categoryController = new CategoryController();
+const technicalFileController = new TechnicalFileController();
+const optionController = new OptionController();
+const colorController = new ColorController();
 
-Project3Router.get('/products', productController.getProducts);
-Project3Router.get('/product/:id', productController.getProductById);
-
+Project3Router.get('/product/:productId', productController.getProductById);
 Project3Router.get('/categories', categoryController.getCategories);
+Project3Router.get(
+  '/categories-by-product/:productId',
+  categoryController.findCategoriesOfProduct
+);
+
 Project3Router.get(
   '/category/:convertName',
   categoryController.getProductsByCategory
+);
+Project3Router.get(
+  '/technical-files/:productId',
+  technicalFileController.getTechnicalFiles
+);
+Project3Router.get('/option/:productId', optionController.getOption);
+Project3Router.get('/color/:productId', colorController.getFixedColor);
+Project3Router.get(
+  '/colors-first-group/:productId',
+  colorController.getColorsOfFirstGroup
+);
+Project3Router.get(
+  '/colors-second-group/:productId',
+  colorController.getColorsOfSecondGroup
 );
 
 export default Project3Router;
