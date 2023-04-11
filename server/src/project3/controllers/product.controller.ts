8b043ef257b0690377cf4eb22c35286edeb9dbea
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-11 18:24:55 +0200

    server: fix product controller, manager & interface

--- server/src/project3/controllers/product.controller.ts
+++ server/src/project3/controllers/product.controller.ts
@@ -1,72 +1,24 @@
 import { Request, Response } from 'express';
 import ProductManager from '../models/product.manager';
-import OptionManager from '../models/option.manager';
 
 class ProductController {
-  constructor(
-    private productManager = new ProductManager(),
-    private optionManager = new OptionManager()
-  ) {}
-
-  public getProducts = async (_req: Request, res: Response) => {
-    return await this.productManager
-      .findAll()
-      .then((result) => {
-        if (!result) {
-          res.sendStatus(404);
-        } else {
-          const product = result.forEach((e) => {
-            if (!e.first_image) {
-              e.first_image = e.id + '.png';
-            }
-          });
-          res.status(200).json(result);
-        }
-      })
-      .catch((err: any) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  };
+  constructor(private productManager = new ProductManager()) {}
 
   public getProductById = async (req: Request, res: Response) => {
-    const id: string = req.params.id;
-    const product = await this.productManager
-      .findOne(id)
+    const productId: string = req.params.productId;
+    return await this.productManager
+      .findOne(productId)
       .then((result) => {
         if (!result) {
           res.sendStatus(404);
         } else {
-          return result;
+          return res.status(200).json(result[0]);
         }
       })
       .catch((err: any) => {
         console.error(err);
         res.sendStatus(500);
       });
-
-    if (product && product[0].option_id !== null) {
-      let options = await this.optionManager
-        .findOne(id)
-        .then((result) => {
-          if (!result) {
-            res.sendStatus(404);
-          } else {
-            const options = {
-              name: product[0].option_name,
-              detail: result,
-            };
-            return options;
-          }
-        })
-        .catch((err: any) => {
-          console.error(err);
-          res.sendStatus(500);
-        });
-      product[0].options = options;
-    }
-
-    return res.status(200).json(product);
   };
 }
 
--- server/src/project3/models/product.manager.ts
+++ server/src/project3/models/product.manager.ts
@@ -1,13 +1,9 @@
-import IProduct from '../interfaces/product';
 import connection from '../database';
 import { Pool } from 'mysql2/promise';
 import { TABLE } from '../enums/table.enum';
+import IProduct from '../interfaces/product';
 
 const product = TABLE.PRODUCT;
-const option_group = TABLE.OPTION_GROUP;
-const product_category = TABLE.PRODUCT_CATEGORY;
-const category = TABLE.CATEGORY;
-const color = TABLE.COLOR;
 
 class ProductManager {
   public connection: Pool;
@@ -15,25 +11,13 @@ class ProductManager {
     this.connection = connection;
   }
 
-  public async findAll(): Promise<IProduct[]> {
-    const sql = `SELECT id, product_name, first_image FROM ${product} ORDER BY product_order`;
-    const [rows] = await this.connection.execute(sql);
-    return rows as IProduct[];
-  }
-
-  public async findOne(id: string): Promise<any[]> {
+  public async findOne(productId: string): Promise<IProduct[]> {
     const sql = `SELECT p.id AS product_id, p.product_name, p.text_introduction, p.text_size, p.mirror, p.price,
-    IFNULL(p.first_image, CONCAT(p.id, ".png")) AS first_image,
-    IF(pc.main_category = 1, c.category_name,'') AS main_category, color.color_name AS fixed_color
+    IFNULL(p.first_image, CONCAT(p.id, ".png")) AS first_image
     FROM ${product} AS p
-    LEFT JOIN ${option_group} AS o ON o.id = p.option_id
-    LEFT JOIN ${product_category} AS pc ON pc.product_id = p.id
-    LEFT JOIN ${category} AS c ON c.id = pc.category_id
-    LEFT JOIN ${color} AS color ON color.id = p.fixed_color_id
-    WHERE pc.main_category = 1
-    AND p.id = ?`;
+    WHERE p.id = ?`;
 
-    const [rows] = await this.connection.execute(sql, [id]);
+    const [rows] = await this.connection.execute(sql, [productId]);
     return rows as any[];
   }
 }
