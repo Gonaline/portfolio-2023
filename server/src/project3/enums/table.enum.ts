eb3f55dbd0ce906ef732190b8f97bba67a9197e1
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-11 15:51:29 +0200

    server: update table enum with product_option

--- server/src/project3/enums/table.enum.ts
+++ server/src/project3/enums/table.enum.ts
@@ -6,5 +6,6 @@ export enum TABLE {
   PRODUCT = 'product',
   PRODUCT_CATEGORY = 'product_category',
   PRODUCT_COLOR = 'product_color',
+  PRODUCT_OPTION = 'product_option',
   TECHNICAL_FILE = 'technical_file',
 }
