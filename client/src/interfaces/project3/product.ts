eb5057f35dd816eeee28157e2231dc1c5ba3f097
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-12 13:02:10 +0200

    client: add text_size to product interface

--- client/src/interfaces/project3/product.ts
+++ client/src/interfaces/project3/product.ts
@@ -2,6 +2,7 @@ export default interface IProduct {
   product_id: string;
   product_name: string;
   text_introduction: string;
+  text_size: string;
   mirror: number;
   price: number;
   first_image: string;
