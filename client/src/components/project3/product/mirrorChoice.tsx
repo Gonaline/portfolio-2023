485848fb623a44414a8a2f42376e8ecc67641b3a
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-17 15:01:01 +0200

    client: create mirrorChoice component

---
+++ client/src/components/project3/product/mirrorChoice.tsx
@@ -0,0 +1,57 @@
+import { FunctionComponentElement, ReactElement } from 'react';
+import { ColorChoiceStyle } from '../../../style/project3/productStyle';
+import { FOLDER } from '../../../enums/project3/folder.enum';
+import { PAGE } from '../../../enums/page.enum';
+import { OPTION } from '../../../enums/project3/option.enum';
+
+interface MirrorChoiceProps {
+  setIsMirror: (c: string) => void;
+  isMirror: string;
+}
+
+const MirrorChoice = ({
+  setIsMirror,
+  isMirror,
+}: MirrorChoiceProps): FunctionComponentElement<ReactElement> => {
+  return (
+    <ColorChoiceStyle>
+      <h5>
+        <em>{`Orientation ? : ${
+          isMirror === OPTION.MIRROR ? 'inversée' : 'non inversée'
+        }`}</em>
+      </h5>
+
+      <div className='choice'>
+        <button
+          className={`${isMirror === OPTION.NO_MIRROR ? 'selected' : ''} `}
+          type='button'
+          value={OPTION.NO_MIRROR}
+          key={OPTION.NO_MIRROR}
+          onClick={() => {
+            setIsMirror(OPTION.NO_MIRROR);
+          }}
+        >
+          <img
+            src={require(`../../../assets/pictures/${PAGE.PROJECT3}/${FOLDER.OPTIONS}/${OPTION.NO_MIRROR}.png`)}
+            alt={OPTION.NO_MIRROR}
+          />
+        </button>
+        <button
+          className={`${isMirror === OPTION.MIRROR ? 'selected' : ''} `}
+          type='button'
+          value={OPTION.MIRROR}
+          key={OPTION.MIRROR}
+          onClick={() => {
+            setIsMirror(OPTION.MIRROR);
+          }}
+        >
+          <img
+            src={require(`../../../assets/pictures/${PAGE.PROJECT3}/${FOLDER.OPTIONS}/${OPTION.MIRROR}.png`)}
+            alt={OPTION.MIRROR}
+          />
+        </button>
+      </div>
+    </ColorChoiceStyle>
+  );
+};
+export default MirrorChoice;
