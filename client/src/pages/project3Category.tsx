72178c8458bde50b226d1018c29fe4dc14256059
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-20 12:12:33 +0200

    client: add CollectionTitle

--- client/src/pages/project3Category.tsx
+++ client/src/pages/project3Category.tsx
@@ -6,20 +6,14 @@ import {
 } from 'react';
 import { useParams } from 'react-router-dom';
 import project3Ctx from '../context/project3Ctx';
-import { CollectionTitleStyle } from '../style/project3/collectionTitleStyle';
 import project3ProductCtx from '../context/project3CtxProduct';
-import { PAGE } from '../enums/page.enum';
-import { FOLDER } from '../enums/project3/folder.enum';
 import ListOfProducts from '../components/project3/listOfProducts';
+import CollectionTitle from '../components/project3/product/collectionTitle';
 
 const Project3Category = (): FunctionComponentElement<ReactElement> => {
   const { collection } = useParams<{ collection?: string }>();
-  const {
-    collectionName,
-    collectionConvertName,
-    setCollectionConvertName,
-    collectionImage,
-  } = useContext(project3Ctx);
+  const { collectionConvertName, setCollectionConvertName } =
+    useContext(project3Ctx);
   const { resetProductData } = useContext(project3ProductCtx);
 
   window.scrollTo(0, 0);
@@ -34,15 +28,7 @@ const Project3Category = (): FunctionComponentElement<ReactElement> => {
 
   return (
     <>
-      <CollectionTitleStyle>
-        <div>
-          <img
-            src={require(`../assets/pictures/${PAGE.PROJECT3}/${FOLDER.CATEGORIES}/${collectionImage}`)}
-            alt='collection'
-          />
-        </div>
-        <h4>{collectionName}</h4>
-      </CollectionTitleStyle>
+      <CollectionTitle />
       <ListOfProducts />
     </>
   );
