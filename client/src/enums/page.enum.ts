7ffd085a71ac69ab7dd08ce664a99f7f4a96968c
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-26 13:01:26 +0200

    client: update page enum with cv

--- client/src/enums/page.enum.ts
+++ client/src/enums/page.enum.ts
@@ -3,8 +3,11 @@ export enum PAGE {
   PROJECT1 = 'project1',
   PROJECT2 = 'project2',
   PROJECT3 = 'project3',
+  CV = 'cv',
+
   ABOUT_PATH = 'about',
   PROJECT1_PATH = 'modelling-gallery',
   PROJECT2_PATH = 'rock-paper-or-scissors',
   PROJECT3_PATH = 'stickers-shop',
+  CV_PATH = 'cv',
 }
