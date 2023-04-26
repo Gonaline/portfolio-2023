d7f26ca2b58fefc7cc9c5bc4ab1755f226eef3bc
Gonaline <aline-gonce_student2022@wilder.school>
2023-04-26 13:00:48 +0200

    client: add cv link to footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import linkedinImg from '../assets/pictures/linkedin-icon.svg';
 import githubImg from '../assets/pictures/github-icon.svg';
 import FooterStyle from '../style/footerStyle';
+import { PAGE } from '../enums/page.enum';
 
 const Footer = (): FunctionComponentElement<ReactElement> => {
   return (
@@ -14,6 +15,9 @@ const Footer = (): FunctionComponentElement<ReactElement> => {
         </Link>
         <Link to='https://www.github.com/Gonaline' target='_blank'>
           <img src={githubImg} alt='github-icon' />
+        </Link>
+        <Link to={PAGE.CV_PATH} className='cv'>
+          <p>CV</p>
         </Link>
       </div>
     </FooterStyle>
